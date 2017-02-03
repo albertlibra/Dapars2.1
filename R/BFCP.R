5073acaabcd528d807d450163ddf5145b6e9fdbe
albertlibra <wjzhou94@foxmail.com>
2017-02-03 09:35:03 -0600

    l

--- R/BFCP.R
+++ R/BFCP.R
@@ -19,13 +19,13 @@ moveColumnToRowName <- function(dat, column=1, enforceMatrix=FALSE){
   if(anyDuplicated(dat[, column])) stop(sprintf('The specified column: %d is not unique!\n', column))
   res <- data.frame(dat[, -column], check.names=FALSE)
   rownames(res) <- dat[, column]
-  if(enforceMatrix) 
+  if(enforceMatrix)
     res <- data.matrix(res)
-  res	
+  res
 }
 Bimodalfitting <- function(y, ...){
-  
-  
+
+
   if(n_unique(y)<4){
     res <- rep(NA, 8)
     names(res) <- c("mu1", "mu2", "sigma1", "sigma2", "pi1", "delta", "DI",'diff')
@@ -43,7 +43,7 @@ n_unique <- function(x, ignoreNA=FALSE){
 }
 noNA <- function (dat, returnIndex = FALSE) {
   sel <- complete.cases(dat)
-  if (returnIndex) 
+  if (returnIndex)
     return(sel)
   if (is.null(dim(dat))) {
     res <- dat[sel]
@@ -54,20 +54,20 @@ noNA <- function (dat, returnIndex = FALSE) {
   res
 }
 SIBER1.1 <- function(y,model='E'){
- 
-  
-  res <- rep(NA, 8) 
-  names(res) <- c('mu1', 'mu2', 'sigma1', 'sigma2', 'pi', 'delta', 'DI','mdiff') 
+
+
+  res <- rep(NA, 8)
+  names(res) <- c('mu1', 'mu2', 'sigma1', 'sigma2', 'pi', 'delta', 'DI','mdiff')
   # if y has NA, all result will be NA; thus need to remove NA beforehand
   fit <- fitNL1(noNA(y), model=model)[1:5]
-  # browser()
+  browser()
   # fit <- fitNL(y, model='V')[1:5]
-  DIinfo <- parToBI(fit)
+  DIinfo <- parToDI(fit)
   res[1:5] <- fit
   res[6:8] <- DIinfo
   res
 }
-parToBI <- function(mat){
+parToDI <- function(mat){
   res <- c(NA, NA, NA)
   names(res) <- c('delta', 'DI','udiff')
   if(!any(is.na(mat[1:5]))){
@@ -85,26 +85,26 @@ parToBI <- function(mat){
 #######################################################################################################
 # for completeness, we also incorporate normal mixture for microarray data. We use NL to denote normal.
 #' Fit Normal Mixture Model
-#' The function fits a two-component normal mixture model. 
+#' The function fits a two-component normal mixture model.
 #'
 #' The parameter estimates from log normal mixture is obtained by taking logarithm and fit normal mixture. We use
 #' mclust package to obtain parameter estimates of normal mixture model. In particular, \eqn{log_{base}(\frac{y+eps}{d})} is used to
-#' fit to normal mixture model. 
-#' 
-#' With this function, three models can be fitted: (1) log normal mixture with equal variance (E model); 
+#' fit to normal mixture model.
+#'
+#' With this function, three models can be fitted: (1) log normal mixture with equal variance (E model);
 #' (2) Generalized Poisson mixture with unequal variance (V model); (3) 0-inflated log normal model.
 #' The 0-inflated log normal has the following density function:
-#' 
+#'
 #' \eqn{P(Y=y)=\pi D(y) + (1-\pi)LN(\mu, \sigma)} where D is the point mass at 0 while \eqn{LN(\mu, \sigma)} is the density
-#' of log normal distribution with mean \eqn{\mu} and variance \eqn{\sigma^2}. 
-#' 
+#' of log normal distribution with mean \eqn{\mu} and variance \eqn{\sigma^2}.
+#'
 #' The rule to fit 0-inflated model is that the observed percentage of count exceeds the user specified threshold. This
 #' rule overrides the model argument (E or V) when observed percentae of zero count exceeds the threshold.
-#' 
+#'
 #' @param y A vector representing the RNAseq raw count.
-#' @param d A vector of the same length as y representing the normalization constant to be applied to the data. For the LN model, the original data would be devided by this vector. 
-#' @param model Character specifying E or V model. E model fits the mixture model with equal variance while V model doesn't put any constraint. 
-#' @return A vector consisting parameter estimates of mu1, mu2, sigma1, sigma2, pi1, logLik and BIC. 
+#' @param d A vector of the same length as y representing the normalization constant to be applied to the data. For the LN model, the original data would be devided by this vector.
+#' @param model Character specifying E or V model. E model fits the mixture model with equal variance while V model doesn't put any constraint.
+#' @return A vector consisting parameter estimates of mu1, mu2, sigma1, sigma2, pi1, logLik and BIC.
 #' @references Tong, P., Chen, Y., Su, X. and Coombes, K. R. (2012). Systematic Identification of Bimodally Expressed Genes Using RNAseq Data. Bioinformatics, submitted.
 #' @export
 fitNL1 <- function(y,d=NULL, model='E') {
@@ -113,10 +113,10 @@ fitNL1 <- function(y,d=NULL, model='E') {
   names(res) <- c('mu1', 'mu2', 'sigma1', 'sigma2', 'pi1', 'logLik', 'BIC') # 1:7
   Dat <- y/d # normalization
   # browser()
-  
+
   mc <- try(Mclust(Dat, G = 2, modelNames = model), silent  = TRUE)
   # browser()
-  
+
   res[1:7] <- extractMclustPar(mc, modelName=model, dat=Dat)
   res
 }
@@ -138,9 +138,9 @@ extractMclustPar <- function(mc, modelName='E',dat=NA){
     # extract logLik
     res[6] <- mc$loglik
     # extract BIC
-    res[7] <- ifelse(modelName=="V", -bic(modelName="V", loglik=mc$loglik, n=mc$n, d=1, G=2), 
+    res[7] <- ifelse(modelName=="V", -bic(modelName="V", loglik=mc$loglik, n=mc$n, d=1, G=2),
                      -bic(modelName="E", loglik=mc$loglik, n=mc$n, d=1, G=2))
-    
+
   }
   res
 }
